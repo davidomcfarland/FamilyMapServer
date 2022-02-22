c4cc0ce9b9ab659adfec26805dabe6542484d117
david <davidomcfarland@gmail.com>
2022-02-22 16:50:59 -0700

    Implementing add method

--- src/main/java/DataAccess/AuthtokenDao.java
+++ src/main/java/DataAccess/AuthtokenDao.java
@@ -1,14 +1,26 @@
 package DataAccess;
 
+import model.Authtoken;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
- * Authtoken DAO - empty for now - use if needed
+ * Authtoken DAO
  */
 public class AuthtokenDao {
     Connection conn;
 
     AuthtokenDao(Connection iconn){
         conn = iconn;
+    }
+
+    public void add(Authtoken newAuth) throws SQLException {
+        String sql = "insert into Authtoken Values (\"" + newAuth.getAuthtoken() + "\", \"" + newAuth.getUsername() + "\")";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.executeUpdate();
+        }
     }
 }
--- src/main/java/model/Authtoken.java
+++ src/main/java/model/Authtoken.java
@@ -17,4 +17,20 @@ public class Authtoken {
         authtoken = iauthtoken;
         username  = iusername;
     }
+
+    public String getAuthtoken() {
+        return authtoken;
+    }
+
+    public void setAuthtoken(String authtoken) {
+        this.authtoken = authtoken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
